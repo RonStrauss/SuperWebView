29ad6c642d683a6435b0382591097aea2fbee8b6
RonStrauss <ashbelthor@gmail.com>
2024-04-08 17:28:09 +0300

    add barcode scanner

--- app/src/main/java/com/roozbehzarei/webview/MainActivity.kt
+++ app/src/main/java/com/roozbehzarei/webview/MainActivity.kt
@@ -1,31 +1,51 @@
 package com.roozbehzarei.webview
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.content.res.Configuration
 import android.graphics.Bitmap
+import android.icu.text.SimpleDateFormat
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.TypedValue
 import android.view.View
+import android.webkit.PermissionRequest
+import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
 import android.webkit.WebResourceError
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.activity.addCallback
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.webkit.WebSettingsCompat
 import androidx.webkit.WebViewFeature
 import com.roozbehzarei.webview.databinding.ActivityMainBinding
+import java.io.File
+import java.io.IOException
+import java.util.Date
+import java.util.Locale
 
 // The URL of the website to be loaded
-private const val WEBSITE = ""
+private const val WEBSITE = "https://mobilityltd.net"
 
 class MainActivity : AppCompatActivity() {
 
+    private var currentPhotoPath: String? = null;
+
     private lateinit var binding: ActivityMainBinding
     private lateinit var webView: WebView
+
+    // Create a global variable to hold the file chooser callback
+    private var mUploadMessage: ValueCallback<Array<Uri>>? = null
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,6 +66,10 @@ class MainActivity : AppCompatActivity() {
             domStorageEnabled = true
             // Disable support for zooming using webView's on-screen zoom controls and gestures
             setSupportZoom(false)
+            // ask for permission to access the camera
+            mediaPlaybackRequiresUserGesture = false
+            javaScriptCanOpenWindowsAutomatically = true
+
         }
         // If dark theme is turned on, automatically render all web contents using a dark theme
         if (WebViewFeature.isFeatureSupported(WebViewFeature.ALGORITHMIC_DARKENING)) {
@@ -60,15 +84,20 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+
+// Use this to launch the file chooser
+        fileChooserActivityResultLauncher.launch(intent)
+
         // Start loading the given website URL
         webView.loadUrl(WEBSITE)
+
 
         // Define Swipe-to-refresh behavior
         binding.root.setOnRefreshListener {
             if (webView.url == null) {
                 webView.loadUrl(WEBSITE)
             } else {
-                webView.reload()
+//                webView.reload()
             }
         }
 
@@ -91,7 +120,7 @@ class MainActivity : AppCompatActivity() {
          * Disable Swipe-to-refresh if [webView] is scrolling
          */
         webView.viewTreeObserver.addOnScrollChangedListener {
-            binding.root.isEnabled = webView.scrollY == 0
+            binding.root.isEnabled = false
         }
 
         /**
@@ -108,6 +137,36 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
     }
+
+private val fileChooserActivityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+    if (result.resultCode == Activity.RESULT_OK) {
+        val data: Intent? = result.data
+        val resultUris: Array<Uri>?
+        if (data == null || data.data == null) {
+            // If there is not data, then we may have taken a photo
+            if (currentPhotoPath != null) {
+                val photoFile = File(currentPhotoPath!!)
+                val photoURI: Uri = FileProvider.getUriForFile(
+                    this@MainActivity,
+                    "com.example.android.fileprovider",
+                    photoFile
+                )
+                resultUris = arrayOf(photoURI)
+                currentPhotoPath = null
+            } else {
+                resultUris = null
+            }
+        } else {
+            resultUris = WebChromeClient.FileChooserParams.parseResult(result.resultCode, data)
+        }
+        mUploadMessage?.onReceiveValue(resultUris)
+        mUploadMessage = null
+    } else {
+        mUploadMessage?.onReceiveValue(null)
+        mUploadMessage = null
+    }
+}
+
 
     private inner class MyWebViewClient : WebViewClient() {
 
@@ -167,6 +226,61 @@ class MainActivity : AppCompatActivity() {
         override fun onProgressChanged(view: WebView?, newProgress: Int) {
             super.onProgressChanged(view, newProgress)
             binding.progressIndicator.progress = newProgress
+        }
+
+        override fun onPermissionRequest(request: PermissionRequest?) {
+            request?.grant(request.resources)
+        }
+
+        override fun onShowFileChooser(
+            webView: WebView?,
+            filePathCallback: ValueCallback<Array<Uri>>?,
+            fileChooserParams: FileChooserParams?
+        ): Boolean {
+// Ensure there's no existing callback
+            if (mUploadMessage != null) {
+                mUploadMessage?.onReceiveValue(null)
+                mUploadMessage = null
+            }
+
+            mUploadMessage = filePathCallback
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if (takePictureIntent.resolveActivity(packageManager) != null) {
+                // Create a file to store the image
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this@MainActivity,
+                        "com.example.android.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    fileChooserActivityResultLauncher.launch(takePictureIntent)
+                }
+            }
+
+            return true
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = java.text.SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
         }
     }
 
