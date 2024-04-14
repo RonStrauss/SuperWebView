e6bbc122462d697b3319e16819c29525fb027557
RonStrauss <ashbelthor@gmail.com>
2024-04-14 18:11:58 +0300

    add permissions bypass cleartext

--- app/src/main/java/com/roozbehzarei/webview/MainActivity.kt
+++ app/src/main/java/com/roozbehzarei/webview/MainActivity.kt
@@ -1,29 +1,34 @@
 package com.roozbehzarei.webview
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
-import android.content.ActivityNotFoundException
+import android.app.DownloadManager
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.content.res.Configuration
 import android.graphics.Bitmap
-import android.icu.text.SimpleDateFormat
 import android.net.Uri
+import android.os.Build
+import android.os.Build.VERSION_CODES
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
-import android.util.TypedValue
-import android.view.View
+import android.util.Log
 import android.webkit.PermissionRequest
 import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
-import android.webkit.WebResourceError
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.activity.addCallback
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.webkit.WebSettingsCompat
@@ -34,12 +39,18 @@ import java.io.IOException
 import java.util.Date
 import java.util.Locale
 
-// The URL of the website to be loaded
-private const val WEBSITE = "https://mobilityltd.net"
+// constants
+private const val WEBSITE = "https://test.mobilityltd.net"
+
+//private const val WEBSITE = "https://mobilityltd.net"
+private const val DOMAIN = "mobilityltd.net"
+private const val REQUEST_IMAGE_CAPTURE = 1
+private const val REQUEST_LOCATION_PERMISSION = 2
+private const val REQUEST_MEDIA_PERMISSION = 3
 
 class MainActivity : AppCompatActivity() {
 
-    private var currentPhotoPath: String? = null;
+    private var currentPhotoPath: String? = null
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var webView: WebView
@@ -47,11 +58,13 @@ class MainActivity : AppCompatActivity() {
     // Create a global variable to hold the file chooser callback
     private var mUploadMessage: ValueCallback<Array<Uri>>? = null
 
+    @RequiresApi(VERSION_CODES.TIRAMISU)
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.root.isEnabled = false
 
         /**
          * Define and configure [webView]
@@ -84,45 +97,58 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        webView.setDownloadListener { url, _, _, mimetype, _ ->
+            // if sdk version under 32, request download through download manager
+            if (Build.VERSION.SDK_INT < VERSION_CODES.Q) {
+                val request = DownloadManager.Request(Uri.parse(url))
+                request.setMimeType(mimetype)
+                val fileName = url.toString().split("/").last().split("%5C").last()
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName)
+                val downloadManager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+                downloadManager.enqueue(request)
+                Log.d("Download", "Downloading: $fileName")
+            } else {
+                // if sdk version 32 and above, request download through webview download manager
+                webView.loadUrl("javascript:window.open('$url')")
+            }
+//            val request = DownloadManager.Request(Uri.parse(url))
+//            request.setMimeType(mimetype)
+//            val fileName = url.toString().split("/").last().split("%5C").last()
+//            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName)
+//            val downloadManager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+//            downloadManager.enqueue(request)
+//            Log.d("Download", "Downloading: $fileName")
+        }
 
-// Use this to launch the file chooser
         fileChooserActivityResultLauncher.launch(intent)
 
-        // Start loading the given website URL
-        webView.loadUrl(WEBSITE)
-
-
-        // Define Swipe-to-refresh behavior
-        binding.root.setOnRefreshListener {
-            if (webView.url == null) {
-                webView.loadUrl(WEBSITE)
-            } else {
-//                webView.reload()
-            }
+        if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.CAMERA), REQUEST_IMAGE_CAPTURE
+            )
+        } else if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ), REQUEST_LOCATION_PERMISSION
+            )
+        } else if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.READ_MEDIA_IMAGES
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.READ_MEDIA_IMAGES), REQUEST_MEDIA_PERMISSION
+            )
+        } else {
+            loadWebView()
         }
-
-        // Theme Swipe-to-refresh layout
-        val spinnerTypedValue = TypedValue()
-        theme.resolveAttribute(
-            com.google.android.material.R.attr.colorPrimary, spinnerTypedValue, true
-        )
-        val spinnerColor = spinnerTypedValue.resourceId
-        binding.root.setColorSchemeResources(spinnerColor)
-
-        val backgroundTypedValue = TypedValue()
-        theme.resolveAttribute(
-            com.google.android.material.R.attr.colorPrimaryContainer, backgroundTypedValue, true
-        )
-        val backgroundColor = backgroundTypedValue.resourceId
-        binding.root.setProgressBackgroundColorSchemeResource(backgroundColor)
-
-        /**
-         * Disable Swipe-to-refresh if [webView] is scrolling
-         */
-        webView.viewTreeObserver.addOnScrollChangedListener {
-            binding.root.isEnabled = false
-        }
-
         /**
          * When navigating back, close the app if there's no previous webpage for [webView] to go back to
          */
@@ -138,34 +164,41 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
     }
 
-private val fileChooserActivityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-    if (result.resultCode == Activity.RESULT_OK) {
-        val data: Intent? = result.data
-        val resultUris: Array<Uri>?
-        if (data == null || data.data == null) {
-            // If there is not data, then we may have taken a photo
-            if (currentPhotoPath != null) {
-                val photoFile = File(currentPhotoPath!!)
-                val photoURI: Uri = FileProvider.getUriForFile(
-                    this@MainActivity,
-                    "com.example.android.fileprovider",
-                    photoFile
-                )
-                resultUris = arrayOf(photoURI)
-                currentPhotoPath = null
+    private val fileChooserActivityResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+                val resultUris: Array<Uri>?
+                if (data == null || data.data == null) {
+                    // If there is not data, then we may have taken a photo
+                    if (currentPhotoPath != null) {
+                        val photoFile = File(currentPhotoPath!!)
+                        val photoURI: Uri = FileProvider.getUriForFile(
+                            this@MainActivity, "com.example.android.fileprovider", photoFile
+                        )
+                        resultUris = arrayOf(photoURI)
+                        currentPhotoPath = null
+                    } else {
+                        resultUris = null
+                    }
+                } else {
+                    resultUris =
+                        WebChromeClient.FileChooserParams.parseResult(result.resultCode, data)
+                }
+                mUploadMessage?.onReceiveValue(resultUris)
+                mUploadMessage = null
             } else {
-                resultUris = null
+                mUploadMessage?.onReceiveValue(null)
+                mUploadMessage = null
             }
-        } else {
-            resultUris = WebChromeClient.FileChooserParams.parseResult(result.resultCode, data)
+
+
         }
-        mUploadMessage?.onReceiveValue(resultUris)
-        mUploadMessage = null
-    } else {
-        mUploadMessage?.onReceiveValue(null)
-        mUploadMessage = null
+
+    private fun loadWebView() {
+        webView.loadUrl(WEBSITE)
+
     }
-}
 
 
     private inner class MyWebViewClient : WebViewClient() {
@@ -177,7 +210,13 @@ private val fileChooserActivityResultLauncher = registerForActivityResult(Activi
         override fun shouldOverrideUrlLoading(
             view: WebView?, request: WebResourceRequest?
         ): Boolean {
-            if (request?.url.toString().contains(WEBSITE)) {
+            if (request?.url.toString().contains(WEBSITE) || request?.url.toString()
+                    .contains(DOMAIN)
+            ) {
+                if (request?.url.toString().contains(".jpg")) {
+                    // handle download through webview download manager
+                    return false
+                }
                 return false
             }
             Intent(Intent.ACTION_VIEW, request?.url).apply {
@@ -186,50 +225,89 @@ private val fileChooserActivityResultLauncher = registerForActivityResult(Activi
             return true
         }
 
+
         // Show progress indicator when a webpage is being loaded
         override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
             super.onPageStarted(view, url, favicon)
-            binding.webView.visibility = View.VISIBLE
-            binding.errorLayout.visibility = View.GONE
-            binding.progressIndicator.visibility = View.VISIBLE
+//            binding.root.isRefreshing = false
         }
 
         // Hide progress indicator when a webpage is finished loading
         override fun onPageFinished(view: WebView?, url: String?) {
             super.onPageFinished(view, url)
-            binding.root.isRefreshing = false
-            binding.progressIndicator.visibility = View.INVISIBLE
+//            binding.root.isRefreshing = false
+//            binding.progressIndicator.visibility = View.INVISIBLE
         }
 
-        // Show error screen if loading a webpage has failed
-        override fun onReceivedError(
-            view: WebView?, request: WebResourceRequest?, error: WebResourceError?
-        ) {
-            super.onReceivedError(view, request, error)
-            binding.webView.visibility = View.GONE
-            binding.errorLayout.visibility = View.VISIBLE
-            binding.root.isEnabled = false
-            binding.retryButton.setOnClickListener {
-                if (view?.url.isNullOrEmpty()) {
-                    view?.loadUrl(WEBSITE)
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_IMAGE_CAPTURE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Permission was granted
+                    loadWebView()
+
                 } else {
-                    view?.reload()
+                    // Permission was denied. Disable the functionality that depends on this permission.
+                    Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show()
                 }
+                return
+            }
+
+            REQUEST_LOCATION_PERMISSION -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Permission was granted
+                    loadWebView()
+                } else {
+                    // Permission was denied. Disable the functionality that depends on this permission.
+                    Toast.makeText(this, "Location permission denied", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+
+            REQUEST_MEDIA_PERMISSION -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Permission was granted
+                    loadWebView()
+                } else {
+                    // Permission was denied. Disable the functionality that depends on this permission.
+                    Toast.makeText(this, "Media permission denied", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+
+            // Handle other permissions if any
+            else -> {
+                // Ignore all other requests.
             }
         }
-
     }
 
     private inner class MyWebChromeClient : WebChromeClient() {
 
+
         // Update the progress of progress indicator when loading a webpage
         override fun onProgressChanged(view: WebView?, newProgress: Int) {
             super.onProgressChanged(view, newProgress)
-            binding.progressIndicator.progress = newProgress
+//            binding.progressIndicator.progress = newProgress
         }
 
         override fun onPermissionRequest(request: PermissionRequest?) {
-            request?.grant(request.resources)
+            if (request == null) {
+                return
+            }
+            runOnUiThread {
+                if (request.origin.toString().contains(WEBSITE)) {
+                    request.grant(request.resources)
+                } else {
+                    request.deny()
+                }
+            }
         }
 
         override fun onShowFileChooser(
@@ -256,9 +334,7 @@ private val fileChooserActivityResultLauncher = registerForActivityResult(Activi
                 // Continue only if the File was successfully created
                 photoFile?.also {
                     val photoURI: Uri = FileProvider.getUriForFile(
-                        this@MainActivity,
-                        "com.example.android.fileprovider",
-                        it
+                        this@MainActivity, "com.example.android.fileprovider", it
                     )
                     takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
                     fileChooserActivityResultLauncher.launch(takePictureIntent)
@@ -267,12 +343,14 @@ private val fileChooserActivityResultLauncher = registerForActivityResult(Activi
 
             return true
         }
+
     }
 
     @Throws(IOException::class)
     private fun createImageFile(): File {
         // Create an image file name
-        val timeStamp: String = java.text.SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val timeStamp: String =
+            java.text.SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
         val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
         return File.createTempFile(
             "JPEG_${timeStamp}_", /* prefix */
